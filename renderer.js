30c7b6c5ba4043a932ca8b61ad8fff5d7c33df27
CongHuyKieuVHS <conghuykieu2005@gmail.com>
2026-04-08 01:06:37 -0400

    added music time

--- renderer.js
+++ renderer.js
@@ -5,8 +5,12 @@ let previousBtn = document.querySelector('.prev-button');
 let playBtn = document.querySelector('.play-button');
 let nextBtn = document.querySelector('.next-button');
 let currentSong = document.createElement('audio');
+let seek_slider = document.querySelector(".seek_slider");
 let volumeSlider = document.querySelector('.volume_slider');
+let currentTime = document.querySelector('.current-time');
+let totalDuration = document.querySelector('.total-duration');
 let songIndex = 0; 
+let updateTimer;
 let isPlaying = false;
 
 
@@ -34,13 +38,15 @@ let musics = [
 
 
 function loadSong(songIndex){
+    clearInterval(updateTimer);
+    resetValues();
     currentSong.src = musics[songIndex].src;
     currentSong.load();
 
     songTitle.textContent = musics[songIndex].name;
     songArtist.textContent = musics[songIndex].artist;
     songArt.style.backgroundImage = `url(${musics[songIndex].img})`;
-
+    updateTimer = setInterval(seekUpdate, 1000);
     currentSong.onended = nextSong; 
 }
 
@@ -85,8 +91,44 @@ function setVolume(){
     currentSong.volume = volumeSlider.value / 100;
 }
 
+function seekTo(){
+    let seekto = currentSong.duration * (seek_slider.value / 100);
+    currentSong.currentTime = seekto;
+}
+
 function closeApp() {
     window.electronAPI.closeWindow();
+}
+
+function seekUpdate() {
+  let seekPosition = 0;
+
+  // Check if the current track duration is a legible number
+  if (!isNaN(currentSong.duration)) {
+    seekPosition = currentSong.currentTime * (100 / currentSong.duration);
+    seek_slider.value = seekPosition;
+
+    // Calculate the time left and the total duration
+    let currentMinutes = Math.floor(currentSong.currentTime / 60);
+    let currentSeconds = Math.floor(currentSong.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(currentSong.duration / 60);
+    let durationSeconds = Math.floor(currentSong.duration - durationMinutes * 60);
+
+    // Add a zero to the single digit time values
+    if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+    if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+    if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+    if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+    // Display the updated duration
+    currentTime.textContent = currentMinutes + ":" + currentSeconds;
+    totalDuration.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
+function resetValues() {
+  currentTime.textContent = "00:00";
+  totalDuration.textContent = "00:00";
+  seek_slider.value = 0;
 }
 
 window.addEventListener('DOMContentLoaded', () => {
